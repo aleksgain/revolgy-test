ca2974c7ed9dd95f6a9d57a01cb22c65b86bcdb6
aleksgain <aleksgain@gmail.com>
2021-02-03 11:10:23 +0300

    Edited main.tf according to TF manual

--- main.tf
+++ main.tf
@@ -1,5 +1,41 @@
 resource "aws_ecs_service" "revolgy-test" {
   name            = "revolgy-test"
-  task_definition = ""
+  task_definition = "aws_ecs_task_definition.revolgy-test.arn"
   launch_type     = "FARGATE"
+}
+
+resource "aws_cloudwatch_log_group" "revolgy-test" {
+  name = "/ecs/revolgy-test"
+}
+
+resource "aws_ecs_task_definition" "revolgy-test" {
+  family = "revolgy-test"
+
+  container_definitions = <<EOF
+  [
+    {
+      "name": "revolgy-test",
+      "image": "aleksgain/revolgy-test:latest",
+      "portMappings": [
+        {
+          "containerPort": 80
+        }
+      ],
+      "logConfiguration": {
+        "logDriver": "awslogs",
+        "options": {
+          "awslogs-region": "eu-west-1",
+          "awslogs-group": "/ecs/revolgy-test",
+          "awslogs-stream-prefix": "ecs"
+        }
+      }
+    }
+  ]
+  EOF
+
+  cpu = 256
+  memory = 512
+  requires_compatibilities = ["FARGATE"]
+
+  network_mode = "awsvpc"
 }
