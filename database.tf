ef0f66e9f9f275256463885c8829ca5e6725ab5e
aleksgain <aleksgain@gmail.com>
2021-02-05 17:16:12 +0300

    Added config for RDS

--- database.tf
+++ database.tf
@@ -1,17 +1,66 @@
-resource "aws_db_instance" "revolgy-test" {
-  engine         = "mysql"
-  engine_version = "5.6.17"
-  instance_class = "db.t1.micro"
-  name           = "initial_db"
-  username       = var.rds-username
-  password       = var.rds-password
-}
+module "db" {
+  source  = "terraform-aws-modules/rds/aws"
+  version = "~> 2.0"
 
-resource "mysql_user" "revolgy-test" {
-  user               = var.db-username
-  plaintext_password = var.db-password
-}
+  identifier = "revolgy-test-db"
 
-resource "mysql_database" "revolgy-test" {
-  name = "ip"
+  engine            = "mysql"
+  engine_version    = "5.7.19"
+  instance_class    = "db.t1.micro"
+  allocated_storage = 5
+
+  name     = "ip"
+  username = var.rds-username
+  password = var.rds-password
+  port     = "3306"
+
+  iam_database_authentication_enabled = true
+
+  vpc_security_group_ids = ["aws_vpc.app-vpc.id"]
+
+  maintenance_window = "Mon:00:00-Mon:03:00"
+  backup_window      = "03:00-06:00"
+
+  monitoring_interval = "30"
+  monitoring_role_name = "RDSMonitoringRole"
+  create_monitoring_role = true
+
+  subnet_ids = [
+    aws_subnet.public.id,
+    aws_subnet.private.id,
+  ]
+
+  family = "mysql5.7"
+
+  major_engine_version = "5.7"
+
+  deletion_protection = false
+
+  parameters = [
+    {
+      name = "character_set_client"
+      value = "utf8"
+    },
+    {
+      name = "character_set_server"
+      value = "utf8"
+    }
+  ]
+
+  options = [
+    {
+      option_name = "MARIADB_AUDIT_PLUGIN"
+
+      option_settings = [
+        {
+          name  = "SERVER_AUDIT_EVENTS"
+          value = "CONNECT"
+        },
+        {
+          name  = "SERVER_AUDIT_FILE_ROTATIONS"
+          value = "37"
+        },
+      ]
+    },
+  ]
 }
--- providers.tf
+++ providers.tf
@@ -2,10 +2,3 @@ provider "aws" {
   region  = var.aws_region
   profile = var.aws_profile
 }
-
-provider "mysql" {
-  version = "~> 1.6"
-  endpoint = aws_db_instance.revolgy-test.endpoint
-  username = var.db-username
-  password = var.db-password
-}
